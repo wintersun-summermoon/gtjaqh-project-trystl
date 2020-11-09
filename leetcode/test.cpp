d568fb921cfc5004d67d795175ad3b01215c77fe
zangrui <zangrui625@163.com>
2020-11-09 10:12:23 +0800

    leetcode 20201109

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <string>
+#include <map>
 #include <algorithm>
 
 using namespace std;
 
-
-class Solution {
+class Solution1 {
 public:
     vector<int> sortByBits(vector<int>& arr) {
         vector<int> bit(10001, 0);
@@ -25,6 +25,28 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        vector<vector<int>> res;
+        if (points.size() == 0) return res;
+        map<int, vector<vector<int>>> data;
+        for (auto point : points) {
+            int length = point[0]*point[0] + point[1]*point[1];
+            data[length].push_back(point);
+        }
+        auto iter = data.begin();
+        while(K>0) {
+            K-=iter->second.size();
+            for (auto t : iter->second) {
+                res.push_back(t);
+            }
+            iter++;
+        }
+        return res;
+
+    }
+};
 
 int main() {
     return 0;
