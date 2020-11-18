b8bece4099eb54a1a55ec3612e94d16eb8e889fe
zangrui <zangrui625@163.com>
2020-11-18 14:32:21 +0800

    20201118

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -304,6 +304,33 @@ public:
     }
 };
 
+class Solution11 {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int i = 0;
+        while (i < n) {
+            int sumOfGas = 0, sumOfCost = 0;
+            int cnt = 0;
+            while (cnt < n) {
+                int j = (i + cnt) % n;
+                sumOfGas += gas[j];
+                sumOfCost += cost[j];
+                if (sumOfCost > sumOfGas) {
+                    break;
+                }
+                cnt++;
+            }
+            if (cnt == n) {
+                return i;
+            } else {
+                i = i + cnt + 1;
+            }
+        }
+        return -1;
+    }
+};
+
 int main() {
 
     vector<int> arr2 = {2,3,1,5,6};
